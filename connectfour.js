06d018f29a8c673e644fcfb9665194a7b67af103
gprophete <garlyprophete30@gmail.com>
2020-04-09 14:26:36 -0400

    add X to box with click

--- connectfour.js
+++ connectfour.js
@@ -1,15 +1,25 @@
 let emptyBoard;
-const playerOne= "X"
-const playerTwo= "O"
+const playerOne = "X"
+const playerTwo = "O"
 const winCombos = [
-    [0,1,2,3],
-    [4,5,6,7],
-    [8,9,10,11],
-    [12,13,14,15],
-    [0,5,10,15],
-    [12,9,6,3],
-    [0,4,8,12],
-    [1,5,9,13],
-    [2,6,10,14],
-    [3,7,11,15],
+    [0, 1, 2, 3],
+    [4, 5, 6, 7],
+    [8, 9, 10, 11],
+    [12, 13, 14, 15],
+    [0, 5, 10, 15],
+    [12, 9, 6, 3],
+    [0, 4, 8, 12],
+    [1, 5, 9, 13],
+    [2, 6, 10, 14],
+    [3, 7, 11, 15],
 ]
+
+const play = document.querySelectorAll('.box')
+
+for (let i = 0; i < play.length; i++) {
+    play[i].addEventListener('click', () => {
+        play[i].innerHTML = playerOne
+    })
+}
+
+
