9ef025d31bd37774d2401f8ce1dbf1a91153abda
gprophete <garlyprophete30@gmail.com>
2020-04-09 12:54:59 -0400

    table updated, winCombos created

--- connectfour.js
+++ connectfour.js
@@ -1,4 +1,15 @@
-let cleanSlate;
+let emptyBoard;
 const playerOne= "X"
 const playerTwo= "O"
-const winCombos = []
+const winCombos = [
+    [0,1,2,3],
+    [4,5,6,7],
+    [8,9,10,11],
+    [12,13,14,15],
+    [0,5,10,15],
+    [12,9,6,3],
+    [0,4,8,12],
+    [1,5,9,13],
+    [2,6,10,14],
+    [3,7,11,15],
+]
