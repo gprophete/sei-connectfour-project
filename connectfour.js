e3a2c6b1f368da2ceb8aef7774e43ad16d2df89a
gprophete <garlyprophete30@gmail.com>
2020-04-10 09:58:17 -0400

    board updated

--- connectfour.js
+++ connectfour.js
@@ -1,18 +1,26 @@
-let emptyBoard;
+for (let i=0; i<16; i++){
+    let emptyBoard= document.createElement('div')
+    emptyBoard.classList.add('box')
+    document.body.appendChild(emptyBoard)
+
+}
+
 const playerOne = "X"
 const playerTwo = "O"
-const winCombos = [
-    [0, 1, 2, 3],
-    [4, 5, 6, 7],
-    [8, 9, 10, 11],
-    [12, 13, 14, 15],
-    [0, 5, 10, 15],
-    [12, 9, 6, 3],
-    [0, 4, 8, 12],
-    [1, 5, 9, 13],
-    [2, 6, 10, 14],
-    [3, 7, 11, 15],
-]
+// const winCombos = [
+//     [0, 1, 2, 3],
+//     [4, 5, 6, 7],
+//     [8, 9, 10, 11],
+//     [12, 13, 14, 15],
+//     [0, 5, 10, 15],
+//     [12, 9, 6, 3],
+//     [0, 4, 8, 12],
+//     [1, 5, 9, 13],
+//     [2, 6, 10, 14],
+//     [3, 7, 11, 15],
+// ]
+
+
 let playerTurn = 'X'
 
 const play = document.querySelectorAll('.box')
@@ -22,13 +30,11 @@ for (let i = 0; i < play.length; i++) {
         if (playerTurn === 'X') {
             play[i].innerHTML = playerOne
             playerTurn = 'O'
-        } else{
+        } else {
             play[i].innerHTML = playerTwo
             playerTurn = 'X'
         }
 
-     })   
+    })
 
 }
-
-
