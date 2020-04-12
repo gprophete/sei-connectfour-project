1372fec501a4d622b98b45c107f12ae549ace064
gprophete <garlyprophete30@gmail.com>
2020-04-11 22:40:51 -0400

    board updated to 42 cells

--- connectfour.js
+++ connectfour.js
@@ -1,4 +1,4 @@
-for (let i = 0; i < 16; i++) {
+for (let i = 0; i < 42; i++) {
     let emptyBoard = document.createElement('div')
     emptyBoard.classList.add('box')
     document.body.appendChild(emptyBoard)
@@ -31,35 +31,51 @@ const play = document.querySelectorAll('.box')
 for (let i = 0; i < play.length; i++) {
     play[i].addEventListener('click', () => {
 
-        if (i < 4) {
-            if (play[i + 12].innerHTML == "") {
+        if (i < 7) {
+            if (play[i + 35].innerHTML == "") {
                 if (playerTurn === 'X') {
-                    play[i + 12].innerHTML = playerOne
+                    play[i + 35].innerHTML = playerOne
                     playerTurn = 'O'
                 } else {
-                    play[i + 12].innerHTML = playerTwo
+                    play[i + 35].innerHTML = playerTwo
                     playerTurn = 'X'
                 }
 
 
-            }else if (play[i + 8].innerHTML == "") {
+            }else if (play[i + 28].innerHTML == "") {
                 if (playerTurn === 'X') {
-                    play[i + 8].innerHTML = playerOne
+                    play[i + 28].innerHTML = playerOne
                     playerTurn = 'O'
                 } else {
-                    play[i + 8].innerHTML = playerTwo
+                    play[i + 28].innerHTML = playerTwo
                     playerTurn = 'X'
                 }
 
-            } else if (play[i + 4].innerHTML == "") {
+            } else if (play[i + 21].innerHTML == "") {
                 if (playerTurn === 'X') {
-                    play[i + 4].innerHTML = playerOne
+                    play[i + 21].innerHTML = playerOne
                     playerTurn = 'O'
                 } else {
-                    play[i+4].innerHTML = playerTwo
+                    play[i + 21].innerHTML = playerTwo
                     playerTurn = 'X'
                 }
-            }else {
+            }else if (play[i + 14].innerHTML == "") {
+                if (playerTurn === 'X') {
+                    play[i + 14].innerHTML = playerOne
+                    playerTurn = 'O'
+                } else {
+                    play[i + 14].innerHTML = playerTwo
+                    playerTurn = 'X'
+                }
+            }else if (play[i + 7].innerHTML == "") {
+                if (playerTurn === 'X') {
+                    play[i + 7].innerHTML = playerOne
+                    playerTurn = 'O'
+                } else {
+                    play[i + 7].innerHTML = playerTwo
+                    playerTurn = 'X'
+                }
+            } else {
                 if (playerTurn === 'X') {
                     play[i].innerHTML = playerOne
                     playerTurn = 'O'
@@ -68,7 +84,6 @@ for (let i = 0; i < play.length; i++) {
                     playerTurn = 'X'
                 }
             }
-
         }
 
     })
