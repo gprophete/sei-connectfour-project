740dd2b57e28fb5210ca027a14c299f4dc739767
gprophete <garlyprophete30@gmail.com>
2020-04-13 10:55:28 -0400

    playerOne win first win combo

--- connectfour.js
+++ connectfour.js
@@ -7,19 +7,6 @@ for (let i = 0; i < 42; i++) {
 
 const playerOne = "X"
 const playerTwo = "O"
-// const winCombos = [
-//     [0, 1, 2, 3],
-//     [4, 5, 6, 7],
-//     [8, 9, 10, 11],
-//     [12, 13, 14, 15],
-//     [0, 5, 10, 15],
-//     [12, 9, 6, 3],
-//     [0, 4, 8, 12],
-//     [1, 5, 9, 13],
-//     [2, 6, 10, 14],
-//     [3, 7, 11, 15],
-// ]
-
 
 
 let playerTurn;
@@ -31,6 +18,7 @@ const play = document.querySelectorAll('.box')
 for (let i = 0; i < play.length; i++) {
     play[i].addEventListener('click', () => {
 
+        // making first move to bottom row
         if (i < 7) {
             if (play[i + 35].innerHTML == "") {
                 if (playerTurn === 'X') {
@@ -42,7 +30,7 @@ for (let i = 0; i < play.length; i++) {
                 }
 
 
-            }else if (play[i + 28].innerHTML == "") {
+            } else if (play[i + 28].innerHTML == "") {
                 if (playerTurn === 'X') {
                     play[i + 28].innerHTML = playerOne
                     playerTurn = 'O'
@@ -59,7 +47,7 @@ for (let i = 0; i < play.length; i++) {
                     play[i + 21].innerHTML = playerTwo
                     playerTurn = 'X'
                 }
-            }else if (play[i + 14].innerHTML == "") {
+            } else if (play[i + 14].innerHTML == "") {
                 if (playerTurn === 'X') {
                     play[i + 14].innerHTML = playerOne
                     playerTurn = 'O'
@@ -67,7 +55,7 @@ for (let i = 0; i < play.length; i++) {
                     play[i + 14].innerHTML = playerTwo
                     playerTurn = 'X'
                 }
-            }else if (play[i + 7].innerHTML == "") {
+            } else if (play[i + 7].innerHTML == "") {
                 if (playerTurn === 'X') {
                     play[i + 7].innerHTML = playerOne
                     playerTurn = 'O'
@@ -82,9 +70,30 @@ for (let i = 0; i < play.length; i++) {
                 } else {
                     play[i].innerHTML = playerTwo
                     playerTurn = 'X'
+
                 }
             }
+            if (play[i + 35].innerHTML == playerOne
+                && play[i + 28].innerHTML == playerOne
+                && play[i + 21].innerHTML == playerOne
+                && play[i + 14].innerHTML == playerOne) {
+                alert('win')
+            }
+            
+            
+
         }
 
+
+
     })
+
+
+
+
+
+
 }
+
+
+
