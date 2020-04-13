9f1bfa7511ff69abc9899acf6efc9b348c1124fb
gprophete <garlyprophete30@gmail.com>
2020-04-13 14:41:54 -0400

    horizontal winning combo playerOne

--- connectfour.js
+++ connectfour.js
@@ -551,7 +551,151 @@ for (let i = 0; i < play.length; i++) {
                 && play[6].innerHTML == playerTwo) {
                 alert('win')
             }
-
+            // Horizontal winning combo playerOne
+            if (play[35].innerHTML == playerOne
+                && play[36].innerHTML == playerOne
+                && play[37].innerHTML == playerOne
+                && play[38].innerHTML == playerOne) {
+                alert('win')
+            }
+            if (play[36].innerHTML == playerOne
+                && play[37].innerHTML == playerOne
+                && play[38].innerHTML == playerOne
+                && play[39].innerHTML == playerOne) {
+                alert('win')
+            }
+            if (play[37].innerHTML == playerOne
+                && play[38].innerHTML == playerOne
+                && play[39].innerHTML == playerOne
+                && play[40].innerHTML == playerOne) {
+                alert('win')
+            }
+            if (play[38].innerHTML == playerOne
+                && play[39].innerHTML == playerOne
+                && play[40].innerHTML == playerOne
+                && play[41].innerHTML == playerOne) {
+                alert('win')
+            }
+            if (play[28].innerHTML == playerOne
+                && play[29].innerHTML == playerOne
+                && play[30].innerHTML == playerOne
+                && play[31].innerHTML == playerOne) {
+                alert('win')
+            }
+            if (play[29].innerHTML == playerTwo
+                && play[30].innerHTML == playerTwo
+                && play[31].innerHTML == playerTwo
+                && play[32].innerHTML == playerTwo) {
+                alert('win')
+            }
+            if (play[30].innerHTML == playerOne
+                && play[31].innerHTML == playerOne
+                && play[32].innerHTML == playerOne
+                && play[33].innerHTML == playerOne) {
+                alert('win')
+            }
+            if (play[31].innerHTML == playerOne
+                && play[32].innerHTML == playerOne
+                && play[33].innerHTML == playerOne
+                && play[34].innerHTML == playerOne) {
+                alert('win')
+            }
+            if (play[21].innerHTML == playerOne
+                && play[22].innerHTML == playerOne
+                && play[23].innerHTML == playerOne
+                && play[24].innerHTML == playerOne) {
+                alert('win')
+            }
+            if (play[22].innerHTML == playerOne
+                && play[23].innerHTML == playerOne
+                && play[24].innerHTML == playerOne
+                && play[25].innerHTML == playerOne) {
+                alert('win')
+            }
+            if (play[23].innerHTML == playerOne
+                && play[24].innerHTML == playerOne
+                && play[25].innerHTML == playerOne
+                && play[26].innerHTML == playerOne) {
+                alert('win')
+            }
+            if (play[24].innerHTML == playerOne
+                && play[25].innerHTML == playerOne
+                && play[26].innerHTML == playerOne
+                && play[27].innerHTML == playerOne) {
+                alert('win')
+            }
+            if (play[14].innerHTML == playerOne
+                && play[15].innerHTML == playerOne
+                && play[16].innerHTML == playerOne
+                && play[17].innerHTML == playerOne) {
+                alert('win')
+            }
+            if (play[15].innerHTML == playerOne
+                && play[16].innerHTML == playerOne
+                && play[17].innerHTML == playerOne
+                && play[18].innerHTML == playerOne) {
+                alert('win')
+            }
+            if (play[16].innerHTML == playerOne
+                && play[17].innerHTML == playerOne
+                && play[18].innerHTML == playerOne
+                && play[19].innerHTML == playerOne) {
+                alert('win')
+            }
+            if (play[17].innerHTML == playerOne
+                && play[18].innerHTML == playerOne
+                && play[19].innerHTML == playerOne
+                && play[20].innerHTML == playerOne) {
+                alert('win')
+            }
+            if (play[7].innerHTML == playerOne
+                && play[8].innerHTML == playerOne
+                && play[9].innerHTML == playerOne
+                && play[10].innerHTML == playerOne) {
+                alert('win')
+            }
+            if (play[8].innerHTML == playerOne
+                && play[9].innerHTML == playerOne
+                && play[10].innerHTML == playerOne
+                && play[11].innerHTML == playerOne) {
+                alert('win')
+            }
+            if (play[9].innerHTML == playerOne
+                && play[10].innerHTML == playerOne
+                && play[11].innerHTML == playerOne
+                && play[12].innerHTML == playerOne) {
+                alert('win')
+            }
+            if (play[10].innerHTML == playerOne
+                && play[11].innerHTML == playerOne
+                && play[12].innerHTML == playerOne
+                && play[13].innerHTML == playerOne) {
+                alert('win')
+            }
+            if (play[0].innerHTML == playerOne
+                && play[1].innerHTML == playerOne
+                && play[2].innerHTML == playerOne
+                && play[3].innerHTML == playerOne) {
+                alert('win')
+            }
+            if (play[1].innerHTML == playerOne
+                && play[2].innerHTML == playerOne
+                && play[3].innerHTML == playerOne
+                && play[4].innerHTML == playerOne) {
+                alert('win')
+            }
+            if (play[2].innerHTML == playerOne
+                && play[3].innerHTML == playerOne
+                && play[4].innerHTML == playerOne
+                && play[5].innerHTML == playerOne) {
+                alert('win')
+            }
+            if (play[3].innerHTML == playerOne
+                && play[4].innerHTML == playerOne
+                && play[5].innerHTML == playerOne
+                && play[6].innerHTML == playerOne) {
+                alert('win')
+            }
         } 
 
 
